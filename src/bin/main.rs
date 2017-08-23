05ff34f3187b1977e4da3e63be101177d6e3d5e7
Leo Okawa Eriscon <leo.ericson@yahoo.se>
2017-08-23 20:48:45 +0200

    Calculate the mandelbrot and put the results in an array for rendering

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,13 +10,14 @@ use std::time;
 use std::thread::sleep;
 use sdl2::pixels;
 use sdl2::render;
+use Mandelbrot::mandelbrot;
 
 
 fn main() {
     let sdl_context = sdl2::init().everything().build().unwrap();
 
-    let screen_width = 200;
-    let screen_height = 200;
+    let screen_width = 1920;
+    let screen_height = 1080;
     let unit = 4_f32 / screen_height as f32;
 
     let window = sdl_context.window("Title", screen_width, screen_height).build().unwrap();
@@ -24,11 +25,21 @@ fn main() {
     let mut renderer = window.renderer().accelerated().build().unwrap();
     let mut drawer = renderer.drawer();
     let mut event_pump = sdl_context.event_pump();
+    let settings = mandelbrot::Mandelbrot {
+        screen_height,
+        screen_width,
+        unit,
+        limit: 200,
+        offset: mandelbrot::Offset::new(0.0, 0.0),
+    };
 
-    draw(&mut drawer, screen_width, screen_height, unit);
+    // draw(&mut drawer, screen_width, screen_height, unit);
 
+    let colors = mandelbrot::generate(&settings);
     loop {
 
+
+        mandelbrot::draw( &mut drawer, &colors);
         event_pump.pump_events();
 
         drawer.present();
@@ -44,22 +55,3 @@ fn main() {
 }
 
 
-fn draw(drawer: &mut render::RenderDrawer, screen_width:u32, screen_height: u32, unit: f32) {
-    //TODO: Put results in an array and then continuosly draw that onto the screen
-    drawer.clear();
-    let mut color = pixels::Color::RGB(0, 0, 0,);
-    for y in 0..screen_height {
-        for x in 0..screen_width {
-            let (a, bi) = (x as f32 * unit, y as f32 * unit);
-            let result = Mandelbrot::iterate( (a - 2.0, bi - 2.0), 1000);
-
-
-            drawer.set_draw_color(match result {
-                None =>  pixels::Color::RGB(0, 0, 0),
-                Some(s) =>  pixels::Color::RGB(255, 255, 255),
-            });
-            let (x , y ) = (x as i32, y as i32);
-            drawer.draw_point(Point {x, y});
-        }
-    }
-}
